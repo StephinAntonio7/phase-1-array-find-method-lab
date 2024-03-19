865f5129945b3b0bc8430dd90de2f46c00e1fae2
Stephin Roberts <stephin.roberts@gmail.com>
2024-03-19 17:35:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin (record){
+    //we're looking for a season in record who's result is W. If not it will find undefined.
+    if (record.find(season=> season.result === "W")===undefined){
+        return undefined
+    } else {
+    return record.find((season) => season.result === "W").year
+  }
+}
